294cc38616a440684735ba7c5958269a1186bea6
Sean Kozen <sgkozen@gmail.com>
2023-04-04 23:21:47 +0900

    Fetch-post and get

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,27 +1,84 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import IngredientForm from './IngredientForm';
-import IngredientList from './IngredientList';
-import Search from './Search';
+import IngredientForm from "./IngredientForm";
+import IngredientList from "./IngredientList";
+import Search from "./Search";
 
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
-  const addIngredientHandler= (ingredient) => {
-    setUserIngredients(prevIngredients => [...prevIngredients, {id: Math.random().toString(), ...ingredient}]);
-  }
+  useEffect(() => {
+    fetch(
+      "https://react-hooks-review-24b11-default-rtdb.asia-southeast1.firebasedatabase.app/ingredients.json"
+    )
+      .then(response => response.json())
+      .then(responseData => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].ingredient.title,
+            amount: responseData[key].ingredient.amount,
+          });
+        }
+        setUserIngredients(loadedIngredients);
+      });
+  }, []);
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  const addIngredientHandler = (ingredient) => {
+    fetch(
+      "https://react-hooks-review-24b11-default-rtdb.asia-southeast1.firebasedatabase.app/ingredients.json",
+      {
+        method: "POST",
+        body: JSON.stringify({ ingredient }),
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        setUserIngredients((prevIngredients) => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient },
+        ]);
+      });
+  };
+
+  const removeIngredientHandler = (ingredientId) => {
+    setUserIngredients((prevIngredients) =>
+      prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
+    );
+  };
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler}/>
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}}/>
+        <IngredientList
+          ingredients={userIngredients}
+          onRemoveItem={removeIngredientHandler}
+        />
       </section>
     </div>
   );
-}
+};
 
 export default Ingredients;
